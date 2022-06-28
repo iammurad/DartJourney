e3c31191bdf7a822c526314095443cb7b511d44c
iammurad <murik8900@gmail.com>
2022-06-29 00:54:19 +0400

    test

--- bin/input.dart
+++ bin/input.dart
@@ -4,5 +4,5 @@ void main() {
   print("Tell me your BirthDate Year");
   var year = int.parse(stdin.readLineSync()!);
   var age = DateTime.now().year - year;
-  print('Your Age- $age');
+  print('Your Age $age');
 }
