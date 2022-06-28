3f9d231ef9c7a3aea18f17f0bc0071a62db5ae25
iammurad <murik8900@gmail.com>
2022-06-29 01:31:32 +0400

    EfficientLengthIterable

---
+++ bin/iterable.dart
@@ -0,0 +1,18 @@
+void main() {
+  List<int> li = [2, 3, 4, 5, 6, 7];
+  List<String> li2 = ['murad', 'Zeynalli'];
+  List<double> li3 = [2.2, 3.6, 4.6, 5.3, 6.4, 7.1];
+  List<dynamic> li4 = [2, 3, 'a', true, 'murad', 7];
+  print(li3[1]);
+
+  for (var element in li) {
+    print(element);
+  }
+  Set<int> s1 = {2, 3, 4, 5, 6, 7, 8};
+  Map<int, String> m = {1: 'a', 2: 'b'};
+  Map<String, double> marks = {
+    'Murad': 100,
+    'Zeynalli': 200,
+  };
+  print(marks['Murad']);
+}
