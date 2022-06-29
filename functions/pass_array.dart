1b2936233db7f1644bbe0177f61e241b636f7b27
iammurad <murik8900@gmail.com>
2022-06-30 00:33:36 +0400

    passing array through function

---
+++ functions/pass_array.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  var a = [2, 2.3, 'Murad', true];
+  var b = [2, 2.3, 'Murad', true];
+  fun(b);
+}
+
+void fun(List<dynamic> list) {
+  for (var i = 0; i < list.length; i++) {
+    print('value: ${i + 1} = ${list[i]}');
+  }
+}
