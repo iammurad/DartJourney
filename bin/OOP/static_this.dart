42789e816f9859fbcdf8c67cf0b95ca072229be7
iammurad <murik8900@gmail.com>
2022-07-09 17:29:53 +0400

    static & this

--- bin/OOP/static_this.dart
+++ bin/OOP/static_this.dart
@@ -2,7 +2,6 @@ void main() {
   // var h1 = Human();
   Human.values("Murad", p_height: 120);
 }
-
 class Human {
   static var name, age, height;
   static void values(String p_name, {int p_age = 20, double p_height = 1.70}) {
